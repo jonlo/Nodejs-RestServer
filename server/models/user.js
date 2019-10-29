6e2be9611b46c8d7f7f0967fd8173d243429b4ab
Jon Lopez de Guereña <jon.ldg@ludus-vr.com>
2019-10-29 21:34:24 +0100

    added error validation using 'mongoose-unique-validator'

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,11 @@
 
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let validRoles = {
+    values: ['ADMIN_ROLE','USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+}
 
 let Schema = mongoose.Schema;
 
@@ -10,6 +16,7 @@ let userSchema = new Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: [true, 'El correo es necesario']
     },
     password: {
@@ -22,7 +29,8 @@ let userSchema = new Schema({
     },
     role: {
         type: String,
-        default: 'USER_ROLE'
+        default: 'USER_ROLE',
+        enum: validRoles
     },
     state: {
         type: Boolean,
@@ -34,4 +42,6 @@ let userSchema = new Schema({
     }
 });
 
-module.exports = mongoose.model('User',userSchema);
+userSchema.plugin(uniqueValidator, {message: '{PATH} debe de ser único'});
+
+module.exports = mongoose.model('User', userSchema);
