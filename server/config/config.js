75ef749edcbeb5db6de4af805033499c35317e56
Jon Lopez de Guereña <jon.ldg@ludus-vr.com>
2019-10-30 18:59:02 +0100

    heroku config vars bug solved

--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,6 @@ let dbName = process.env.dbName ? process.env.dbName : "test";
 process.env.NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : "dev";
 
 
-let urlDb = process.env.NODE_ENV === "dev" ? `Localhost mongodb://localhost/${dbName}` :`mongodb+srv://${process.env.user}:${process.env.pass}@mongodbnodejsrestserver-9yeyh.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+let urlDb = process.env.NODE_ENV === "dev" ? `Localhost mongodb://localhost/${dbName}` :`mongodb+srv://${process.env.userName}:${process.env.pass}@mongodbnodejsrestserver-9yeyh.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 process.env.urlDB = urlDb;
