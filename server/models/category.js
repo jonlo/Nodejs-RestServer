9b169eb513610328764e629e3ac4d1b3fec50bec
Jon Lopez de Guereña <jon.ldg@ludus-vr.com>
2019-11-10 16:29:41 +0100

    adding categories

---
+++ server/models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let categorySchema = new Schema({
+    description: {
+        type: String,
+        required: [true, 'El nombre es necesario'],
+        unique: [true, 'categories must be unique']
+    },
+    user: {
+        type: Schema.type.ObjectId,
+        ref: 'User'
+    },
+});
+
+
+module.exports = mongoose.model('Category', categorySchema);
